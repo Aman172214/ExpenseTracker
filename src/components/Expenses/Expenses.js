7b300442c894125a085c606b7f1bb36a0b229689
Aman <Sharmaamqn@gmail.com>
2023-05-27 23:47:03 +0530

    add functionality to display and hide form

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -13,6 +13,7 @@ function Expenses(props) {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
+
   return (
     <div>
       <Card className="expenses">
@@ -20,14 +21,7 @@ function Expenses(props) {
           selected={filteredYear}
           onFilterChange={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,34 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
-  const saveExpenseDataHandler = enteredExpenseData =>{
-    const expenseData={
-      ...enteredExpenseData,
-      id: Math.random().toString()
-    }
-    props.onAddExpense(expenseData);
+  const [displayForm, setDisplayForm] = useState(false);
+
+  const displayFormHandler = () => {
+    setDisplayForm(true);
+  };
+  const hideFormHandler = () =>{
+    setDisplayForm(false);
   }
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setDisplayForm(false);
+  };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+      {!displayForm && (
+        <button onClick={displayFormHandler}>Add Expense</button>
+      )}
+      {displayForm && (
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={hideFormHandler} />
+      )}
     </div>
   );
 };
